fd79eb9b957ff351c878ae5050f92b9076eaacaf
unknown <shravyak094@gmail.com>
2020-09-03 13:05:31 +0530

    Added: Singlet.sh file

--- Singlet.sh
+++ Singlet.sh
@@ -1,20 +1,22 @@
 #! /bin/bash
-heads=0
-tails=0
 declare -A singlets
+singlets[H]=0
+singlets[T]=0
 for((count=1; count<=10; count++))
 do
 	coinFlip=$((RANDOM%2))
-	singlets[$count]=$coinFlip
+	if [ $coinFlip -eq 0 ]
+	then
+		value="${singlets[H]}"
+		((value++))
+		singlets[H]=$value
+	else
+		value="${singlets[T]}"
+      ((value++))
+      singlets[T]=$value
+	fi
 done
 for key in "${!singlets[@]}"
 do
-	if [ "${singlets[$key]}" -eq 0 ]
-	then
-		((heads++))
-	else
-		((tails++))
-	fi
+	echo "Percentage of $key: $((${singlets[$key]}*10))"
 done
-echo "Percentage of heads: $(($heads*10))"
-echo "Percentage of tails: $(($tails*10))"
